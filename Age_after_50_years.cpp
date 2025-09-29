806c62f3a6271e2168b6a96a432fcf63e34c5ce4
hamza01AI <hamza.mahboob01@gmail.com>
2025-09-29 22:32:54 +0500

    Initial commit

---
+++ Age_after_50_years.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int age;
+cout<< "Enter your age:"<<endl;
+cin>> age;
+age= age +50;
+cout<< "Your age after 50 years will be: "<< age << endl;
+
+
+
+return 0;
+}
