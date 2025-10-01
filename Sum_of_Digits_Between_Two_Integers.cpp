d4dcac611d9d22dc4fd3a88e6772470bc0fbc15c
hamza01AI <hamza.mahboob01@gmail.com>
2025-10-01 20:00:50 +0500

    Initial Commit

---
+++ Sum_of_Digits_Between_Two_Integers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+cout<< "Enter Two Numbers: ";
+int a,b;
+int sum;
+int x;
+int add;
+cin>>a>>b;
+for(int i=1;i<=b-a;i++){
+ 
+  int j= i+a;
+ add=j+(j+1);
+ 
+  x=add;
+  sum= add+x; 
+
+
+
+}
+
+
+
+
+cout<< "Sum of Digits Between Two Integers is: " << sum << endl;
+
+
+
+
+
+}
