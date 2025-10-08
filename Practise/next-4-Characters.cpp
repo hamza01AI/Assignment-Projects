a39b1f3b2d4031ce0c429db5f8060934be26e451
hamza01AI <hamza.mahboob01@gmail.com>
2025-10-08 22:43:36 +0500

    Inital Commit

---
+++ Practise/next-4-Characters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+cout<<"Enter the character";
+char ch;
+cin>>ch;
+char ch1 = 'a-1';
+int sum;
+for(int i=1; i<=sum;i++){
+ 
+    sum = ch-ch1;
+
+}
+cout<<""<<sum<<endl;
+}
