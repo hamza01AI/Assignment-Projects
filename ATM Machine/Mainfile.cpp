34257af9670106a608392daabec88648e704542f
hamza01Al <hamza.mahboob01@gmail.com>
2025-10-13 18:55:03 +0500

    initial commit

---
+++ ATM Machine/Mainfile.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int balance=20000; // global variable
+int usernumber;
+
+void userselect()
+{
+cout<<"Select Your Account";
+cout<<"1: Hamza /n 2: Abdul Hannan";
+cin>>usernumber;
+
+
+}
+
+void checkbalance(){
+
+cout<<"This Is Your Current Balance : "<<balance;
+
+}
+
+void withdraw()
+{
+int x;
+int y; 
+cout<<"1. Fast Cash           2. Withdraw"<<endl;
+cin>>x;
+if(x==1)
+{
+
+
+int z[4]={1000,5000,10000,15000};   
+cout<<endl<<endl;
+cout<<"1. 1000         2. 5000\n"<<"3. 10000        4. 15000"<<endl;
+cout<<"Enter Number :";
+cin>>z[4];
+switch(z[4]){
+    
+    case 1:
+    balance= balance-1000;
+    cout<<"Your balance is  "<<balance;
+    break;
+    
+    case 2:
+    balance= balance-5000;
+    cout<<"Your balance is  "<<balance;
+    break;
+
+    case 3:
+    balance= balance-10000;
+    cout<<"Your balance is  "<<balance;
+    break;
+
+    case 4:
+    balance= balance-15000;
+    cout<<"Your balance is  "<<balance;
+    break;
+}
+}
+else if(x==2)
+{   while(true){
+    cout<<"Enter the amount of money you want to withdraw :"<<endl;
+    cin>>y;
+    if(y>balance){cout<<"You dont have enough balance, Enter Money again"<<endl;
+    continue;}
+    cout<<"Your balance is :"<<balance-y;
+    break;s
+}}
+
+
+
+//balance = balance - money;
+
+
+
+}
+
+
+
+int main()
+{
+system("cls");
+cout<<"-----WELCOME TO ABC ATM MACHINE-----"<<endl;
+
+withdraw();
+
+
+
+}
